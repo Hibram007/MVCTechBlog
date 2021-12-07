206f3333e3674b29129b95b3f558545e10c35a5b
Hibram007 <87105978+Hibram007@users.noreply.github.com>
2021-12-07 12:39:26 -0700

    Schema file written

---
+++ models/index.js
@@ -0,0 +1,26 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+// check primary and foreign key linkage - review project
+
+Post.belongsTo(User, {
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'postId',
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
+});
+
+module.exports = {
+  User,
+  Comment,
+  Post
+};
