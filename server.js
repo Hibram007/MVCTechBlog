330f2211ff1dc39bb03ec2374f6b48f66917e6dc
Hibram007 <87105978+Hibram007@users.noreply.github.com>
2021-12-10 15:43:40 -0700

    package json modified

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 
-const routes = require('./controllers/');
+const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
 const app = express();
@@ -36,6 +36,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(routes);
 app.use(require('./controllers/'));
 
 app.listen(PORT, () => {
