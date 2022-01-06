9c72d6d8bcb5698b94b0fc3f75cdadfda8b74a01
Hibram007 <87105978+Hibram007@users.noreply.github.com>
2022-01-06 15:39:06 -0700

    API routes updated and  capitalization standardized

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,12 +1,11 @@
-// serves to collect all the API routes and package them up
 const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const postRoutes = require('./postRoutes');
+const commentRoutes = require('./commentRoutes');
 
-const userRoutes = require('./user-routes');
-const commentRoutes = require('./comment-routes');
-const postRoutes = require('./post-routes');
 
-router.use('/users', userRoutes);
+router.use('/user', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
 
-module.exports = router; 
+module.exports = router;
